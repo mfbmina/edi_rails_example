716238aaf89266f5ec60af36d6032d921b85f175
Matheus Mina <mfbmina@gmail.com>
2015-11-09 23:06:43 -0200

    save xml into a file

--- app/workers/store_worker.rb
+++ app/workers/store_worker.rb
@@ -3,8 +3,15 @@ class StoreWorker
 
   def perform(id)
     shipment = Shipment.find(id)
+    xml = shipment.to_xml(include: :products, skip_instruct: true, root: Shipment)
+    save_xml("#{Rails.root}/requests/shipment_#{shipment.id}.xml", xml)
     conn = Net::HTTP.new('192.168.1.100', '3000')
-    response  = conn.post("/xml", shipment.to_xml(include: :products, skip_instruct: true, root: Shipment))
+    response  = conn.post("/xml", xml)
     shipment.update_from_xml(response.body)
+    save_xml("#{Rails.root}/requests/storage#{shipment.storage_number}.xml", response.body)
+  end
+
+  def save_xml(path, xml)
+    File.open(path, 'w') { |file| file.write(xml) }
   end
 end
