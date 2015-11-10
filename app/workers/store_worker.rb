28a497a54edd9de298220deb52ffdf3b6746f370
Matheus Mina <mfbmina@gmail.com>
2015-11-10 20:49:37 -0200

    small fixs on server ip and views

--- app/workers/store_worker.rb
+++ app/workers/store_worker.rb
@@ -3,12 +3,12 @@ class StoreWorker
 
   def perform(id)
     shipment = Shipment.find(id)
-    xml = shipment.to_xml(include: :products, skip_instruct: true, root: Shipment)
+    xml = shipment.to_xml(include: :products, root: Shipment)
     save_xml("#{Rails.root}/requests/shipment_#{shipment.id}.xml", xml)
-    conn = Net::HTTP.new('192.168.1.100', '3000')
+    conn = Net::HTTP.new('200.235.95.196', '3000')
     response  = conn.post("/xml", xml)
     shipment.update_from_xml(response.body)
-    save_xml("#{Rails.root}/requests/storage#{shipment.storage_number}.xml", response.body)
+    save_xml("#{Rails.root}/requests/storage_#{shipment.id}_#{shipment.storage_number}.xml", response.body)
   end
 
   def save_xml(path, xml)
